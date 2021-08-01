0a53d27587356d04d4f1d043649c499d8db905ea
Silverpawn <15199600+Silverpawn@users.noreply.github.com>
2021-08-01 22:07:52 +0100

    Update octoprice_main_inky_with_lametric.py

--- octoprice_main_inky_with_lametric.py
+++ octoprice_main_inky_with_lametric.py
@@ -428,10 +428,7 @@ else: #high res display
 	#			inky.set_pixel(x, y, CLEAN)
 	#			inky.show()
 	#			time.sleep(1.0)
-	for y in range(inky.height - 1):
-		for x in range(inky.width - 1):
-			inky.set_pixel(x, y, 1)
-	inky.show()
+	image = Image.new("RGB", (inky.width, inky.height), (255, 255, 255))
 
 	inky_display.h_flip = False		
 	inky_display.v_flip = False
